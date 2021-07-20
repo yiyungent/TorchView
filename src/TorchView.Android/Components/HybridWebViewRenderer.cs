d79b01c3a26d861d0c5b7445784cad61df569254
yiyun <yiyungent@gmail.com>
2021-07-20 21:09:54 +0800

    feat(torchview.android): android.Webkit.WebView, WebChromeClient, WebSettings config

--- src/TorchView.Android/Components/HybridWebViewRenderer.cs
+++ src/TorchView.Android/Components/HybridWebViewRenderer.cs
@@ -24,15 +24,38 @@ namespace TorchView4Droid.Components
 
             if (e.OldElement != null)
             {
+                // Unsubscribe from event handlers
+
                 Control.RemoveJavascriptInterface("jsBridge");
                 ((HybridWebView)Element).Cleanup();
             }
             if (e.NewElement != null)
             {
-                Control.SetWebViewClient(new JavascriptWebViewClient(this, $"javascript: {JavascriptFunction}"));
+                // Configure the native control and subscribe to event handlers
+
+                // 用于在 Chrome DevTools 调试 WebView
+                Android.Webkit.WebView.SetWebContentsDebuggingEnabled(true);
+
+                // WebViewClient
+                var webViewClient = new JavascriptWebViewClient(this, $"javascript: {JavascriptFunction}");
+                Control.SetWebViewClient(webViewClient);
                 Control.AddJavascriptInterface(new JSBridge(this), "jsBridge");
 
-                // TODO: 改为加载网络资源, 而不是本地
+
+                // WebSettings
+                // 设置渲染优先级
+                Control.Settings.SetRenderPriority(Android.Webkit.WebSettings.RenderPriority.High);
+                //Control.Settings.CacheMode = Android.Webkit.CacheModes.NoCache;
+                // 如果内容已经存在cache 则使用cache，即使是过去的历史记录。如果cache中不存在，从网络中获取
+                //Control.Settings.CacheMode = Android.Webkit.CacheModes.CacheElseNetwork;
+
+
+                // WebChromeClient
+                var webChromeClient = new MyWebChromeClient();
+                Control.SetWebChromeClient(webChromeClient);
+
+                
+                // 改为加载网络资源, 而不是本地
                 //Control.LoadUrl($"file:///android_asset/Content/{((HybridWebView)Element).Uri}");
                 Control.LoadUrl($"{((HybridWebView)Element).Uri}");
             }
---
+++ src/TorchView.Android/Components/MyWebChromeClient.cs
@@ -0,0 +1,35 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Webkit;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TorchView4Droid.Components
+{
+    public class MyWebChromeClient : Android.Webkit.WebChromeClient
+    {
+
+        public override void OnProgressChanged(WebView view, int newProgress)
+        {
+            // TODO: WebView 顶部进度条
+            // https://blog.csdn.net/wuqingsen1/article/details/82622010
+            //显示进度条
+            //progressBar.setProgress(newProgress);
+            //if (newProgress == 100)
+            //{
+            //    //加载完毕隐藏进度条
+            //    progressBar.setVisibility(View.GONE);
+            //}
+
+
+            base.OnProgressChanged(view, newProgress);
+        }
+
+    }
+}
